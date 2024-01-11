7d0019673f0a8cc87eded0de3f06fe0e5fa6a183
Rafael Pimenta <rafaelre7@gmail.com>
2024-01-11 12:22:06 -0300

    Implementando listView e padding

--- lib/main.dart
+++ lib/main.dart
@@ -15,84 +15,73 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Tarefas'),
+          ),
+          body: ListView(
+            // scrollDirection: Axis.horizontal, //caso queira fazer padrao carrosel na horizonta
             children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    height: 50,
-                    width: 50,
-                  )
-                ],
-              ),
-              Container(
-                color: Colors.amber,
-                height: 30,
-                width: 300,
-                child: Text(
-                  'Diamante amarelo',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print('Você apertou o botão');
-                  },
-                  child: Text('Aperte o botão !'),
-              ),
+              Task('Aprender Flutter'),
+              Task('Aprender Java'),
+              Task('Aprender Kotlin'),
+              Task('Aprender Kotlin'),
+              Task('Aprender Kotlin hkjsefhdjkfhgdsjkhgkjdshgkjhsdkjghkjsdhgkjdfhgjha'),
+              Task('Aprender Kotlin'),
+              Task('Aprender Kotlin'),
             ],
           ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+          ),
         ));
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.grey,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                      child: Text(
+                    nome,
+                    style: TextStyle(
+                      fontSize: 24,
+                      overflow: TextOverflow.ellipsis //colocando ... caso texto muito grande
+                    ),
+                  )),
+                  ElevatedButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
